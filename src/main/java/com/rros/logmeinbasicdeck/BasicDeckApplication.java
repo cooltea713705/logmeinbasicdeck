81cc8449d0ba5d7db7b45b13459e9c511f35b6b7
Raymond Ros <raymond.ros@gmail.com>
2020-12-14 12:54:42 -0500

    Initialized with Spring initializr

---
+++ src/main/java/com/rros/logmeinbasicdeck/BasicDeckApplication.java
@@ -0,0 +1,13 @@
+package com.rros.logmeinbasicdeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicDeckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicDeckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rros/logmeinbasicdeck/BasicDeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rros.logmeinbasicdeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicDeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
